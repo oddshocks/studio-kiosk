cc0aa1a67162479abba572766a2eb1127f0b1ba6
David Gay <oddshocks@riseup.net>
2013-12-13 16:33:53 -0500

    Get all four of our latest tweets.

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ studio-kiosk
 ============
 
 Flask-powered kiosk for use on the Debian server I just set up at our place.
+
+## Requirements
+
+*   pip install flask
+*   pip install twitter
+
+## Setup
+
+You must `mv secrets.py.example secrets.py` and set your own
+Twitter application credentials.
--- kiosk.py
+++ kiosk.py
@@ -1,10 +1,25 @@
+import os
+
 from flask import Flask, render_template
+from twitter import *
 
 app = Flask(__name__)
 
+execfile("secrets.py")
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    TWITTER_CREDS = os.path.expanduser('~/.twitter_creds')
+    if not os.path.exists(TWITTER_CREDS):
+        oauth_dance("studio-kiosk", CONSUMER_KEY,
+                    CONSUMER_SECRET, TWITTER_CREDS)
+
+    oauth_token, oauth_secret = read_token_file(TWITTER_CREDS)
+
+    twitter = Twitter(auth=OAuth(
+                oauth_token, oauth_secret, CONSUMER_KEY, CONSUMER_SECRET))
+
+    return render_template("index.html", twitter=twitter)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
